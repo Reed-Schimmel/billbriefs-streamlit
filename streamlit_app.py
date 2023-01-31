dcda4ccaf9dbfdd7940a89dbdcfdfab5ecd332fe
Reed Schimmel <reedschimmel@gmail.com>
2023-01-30 18:42:00 -0600

    move secrets to .env

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,28 @@
 import streamlit as st
+from congress import Congress
+from dotenv import load_dotenv, dotenv_values
+# Config webapp
+st.set_page_config(
+    # page_title="Best TA Demo",
+    # page_icon=":chart_with_upwards_trend:", # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+    # layout="wide",
+    # menu_items={
+    #     'Get Help': FAQ_URL,
+    #     'Report a bug': BUG_REPORT_URL,
+    #     'About': "# This is a header. This is an *extremely* cool app!" # TODO
+    # },
+)
 
-st.write("Hello World")
+### Main App Logic
+env_config = dotenv_values(".env")
+PROPUBLICA_API_KEY = env_config["PROPUBLICA_API_KEY"]
+DETA_API_KEY = env_config["DETA_API_KEY"]
+DETA_ID = env_config["DETA_ID"]
+
+# Chamber
+SENATE = 'senate'
+HOUSE = 'house'
+
+
+st.write("I want to summize the voting records for each member of congress using GPT and ProPublica")
+st.write(env_config)
