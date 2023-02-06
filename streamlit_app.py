b02bb03755cc634f659560c22d2b4303817b55d2
Jacob Horton <jacob.horton@ku.edu>
2023-02-06 17:45:11 -0600

    Member Cards

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from congress import Congress
+from datetime import datetime, timedelta
+
+from const import STATE_DICT
 
 # Config webapp
 st.set_page_config(
@@ -37,13 +40,35 @@ def get_current_house_members():
     # https://propublica-congress.readthedocs.io/en/latest/api.html#module-congress.members
     return congress.members.filter(HOUSE)[0]["members"]
 
+@st.cache(ttl=60*60*24)
+def calculate_age(birthdate):
+    today = datetime.now()
+    birthdate = datetime.strptime(birthdate, '%Y-%m-%d')
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return age
+
+def render_member(member):
+    container = st.container()
+    container.image(f"https://www.congress.gov/img/member/{member['id'].lower()}_200.jpg")
+    container.subheader(member["short_title"] + " " + member["first_name"] + " " + member["last_name"])
+    container.text("State: " + STATE_DICT[member["state"]])
+    if "district" in member:
+        container.text("District: " + member["district"])
+    container.text(f"Age: {calculate_age(member['date_of_birth'])}")
+    container.text("Next Election: " + member["next_election"])
+    container.button("Voting Record", key=member["id"])
+    
 # WEBAPP
 # st.write("I want to summize the voting records for each member of congress using GPT and ProPublica")
 
-st.header("Members")
-st.subheader("Senate")
+st.title("Members")
+st.header("Senate")
 senate_members = get_current_senate_members()
-st.write(senate_members)
-st.subheader("House")
+#st.write(senate_members)
+for senator in senate_members[:5]:
+    render_member(senator)
+st.header("House")
 house_members = get_current_house_members()
-st.write(house_members)
+for rep in house_members[:5]:
+    render_member(rep)
+#st.write(house_members)
