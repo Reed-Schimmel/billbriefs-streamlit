9730187289a56c1e73ec6f9043e92d36049a22b3
Reed Schimmel <reedschimmel@gmail.com>
2023-02-23 11:06:19 -0600

    misc

--- pages/Bills.py
+++ pages/Bills.py
@@ -32,7 +32,7 @@ HOUSE = 'house'
 congress = Congress(PROPUBLICA_API_KEY)
 
 st.title("ProPublica Bill Viewer")
-bill_type = st.selectbox("Type of bills to search for.", ["Passed"])
+bill_type = st.selectbox("Type of bills to search for.", ["Passed"])#, "By Member"])
 chamber = st.selectbox("Chamber", [HOUSE, SENATE])
 congress_num = st.number_input("Congress number (Current 118)", min_value=110, max_value=118, value=117)
 
@@ -41,6 +41,8 @@ congress_num = st.number_input("Congress number (Current 118)", min_value=110, m
 def get_bills(bill_type, chamber, congress_num):
     if bill_type == "Passed":
         return congress.bills.passed(chamber=chamber, congress=congress_num)
+    # if bill_type == "By Member":
+    #     return congress.bills.by_member(chamber=chamber, congress=congress_num)
 
 if st.button("Search"):
     data = get_bills(bill_type, chamber, congress_num)
