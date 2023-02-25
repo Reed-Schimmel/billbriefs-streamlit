bc17994afca56950b74c6f2a682d0bf414b5d9af
Jacob Horton <99301327+hortojac@users.noreply.github.com>
2023-02-25 16:58:20 -0600

    Address (#8)
    * Update
    
    * New Keys
    
    * Search ZipCode
    
    * Search by Address
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Updated Search Box
    
    * UI Changes
    
    * Photo Fix for Reps
    
    * Sort by District and fixed name search
    
    * Image Fix, Address Search Page Rename
    
    * New Layout
    
    * Comments and New Layout
    
    * Next Election Info Fix
    
    * ignore secrets everywhere
    
    ---------
    
    Co-authored-by: Reed Schimmel <reedschimmel@gmail.com>

---
+++ pages/Address_Search.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import requests
+import re
+from streamlit_extras.switch_page_button import switch_page
+
+# Config webapp
+st.set_page_config(
+    page_title="BillBriefs",
+    # page_icon=":chart_with_upwards_trend:", # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+    # layout="wide",
+    # menu_items={
+    #     'Get Help': FAQ_URL,
+    #     'Report a bug': BUG_REPORT_URL,
+    #     'About': "# This is a header. This is an *extremely* cool app!" # TODO
+    # },
+    initial_sidebar_state="collapsed"
+)
+
+@st.cache_data
+def google_geocode_requests(search_address):
+    GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+
+    # Set the API endpoint URL and API key
+    url = 'https://www.googleapis.com/civicinfo/v2/representatives'
+
+    # Define the API parameters, including the address and API key
+    params = {
+        'address': search_address,
+        'key': GOOGLE_API_KEY
+    }
+
+    # Send an HTTP GET request to the API endpoint with the specified parameters
+    response = requests.get(url, params=params)
+
+    # Retrieve the JSON response from the API
+    response_data = response.json()
+    return response_data
+
+address_regex = r'\b\d+\s+\w+\s+\w+\b'
+search_address = st.text_input("Search", placeholder="Search by address.")
+if re.search(address_regex, search_address):
+    # Use the coordinates to look up the representatives for that location
+    response_data = google_geocode_requests(search_address)
+    st.write(response_data)
+    # Extract the relevant representative information from the response
+    officials = response_data['officials']
+    offices = response_data['offices']
+
+    # Process and display the representative information
+    for office in offices:
+        for index in office['officialIndices']:
+            official = officials[index]
+            title = office['name']
+            if "U.S. Senator" in title or "U.S. Representative" in title:
+                st.text('Name: %s, Title: %s' % (official['name'], title))
+
+
--- pages/Member_List.py
+++ pages/Member_List.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import requests
 from congress import Congress
-from datetime import datetime, timedelta
+from datetime import datetime
 from streamlit_extras.switch_page_button import switch_page
 
 from const import STATE_DICT
@@ -57,24 +58,63 @@ def render_member(member):
         st.session_state["selected_member"] = member
 
     container = st.container()
-    container.image(f"https://www.congress.gov/img/member/{member['id'].lower()}_200.jpg")
+    # Temporary fix for missing image for Senator Pete Ricketts
+    if member["id"] == "R000618":
+        container.image("https://upload.wikimedia.org/wikipedia/commons/b/bd/Sen._Pete_Ricketts_official_portrait%2C_118th_Congress.jpg", width=200)
+    elif "Representative" in member["title"]:
+        container.image(f"https://clerk.house.gov/content/assets/img/members/{member['id']}.jpg", width=200)
+    elif "Senator" in member["title"]:
+        container.image(f"https://www.congress.gov/img/member/{member['id'].lower()}_200.jpg", width=200)
     container.subheader(member["short_title"] + " " + member["first_name"] + " " + member["last_name"])
-    container.text("State: " + STATE_DICT[member["state"]])
+    #container.text("State: " + STATE_DICT[member["state"]])
     if "district" in member:
         container.text("District: " + member["district"])
     container.text(f"Age: {calculate_age(member['date_of_birth'])}")
     if "next_election" in member:
         container.text("Next Election: " + member["next_election"])
+    else:
+        container.text("Next Election: N/A")
     if container.button("Voting Record", key=member["id"], on_click = set_this_member):
         switch_page("Voting_Record")
 
 @st.cache_data
+def google_geocode_requests(search_address):
+    GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+
+    # Set the API endpoint URL and API key
+    url = 'https://www.googleapis.com/civicinfo/v2/representatives'
+
+    # Define the API parameters, including the address and API key
+    params = {
+        'address': search_address,
+        'key': GOOGLE_API_KEY
+    }
+
+    # Send an HTTP GET request to the API endpoint with the specified parameters
+    response = requests.get(url, params=params)
+
+    # Retrieve the JSON response from the API
+    response_data = response.json()
+    return response_data
+
+@st.cache_data
 def search_members(search_by, member):
-    '''Returns True if search_by is in member's name or state name'''
+    '''Returns True if search_by is in member's name, state name, or address'''
+
+    # Search by name or state
     if search_by.lower() in (member['first_name'] + " " + member['last_name']).lower():
         return True
     elif search_by.lower() in [value.lower() for value in STATE_DICT.values()] and member["state"] in STATE_DICT.keys() and STATE_DICT[member["state"]].lower() == search_by.lower():
         return True
+
+    response_data = google_geocode_requests(search_by)
+    officials = response_data.get('officials', [])
+    if officials:
+        for official in officials:
+            if (member['first_name'].lower() in official['name'].lower()) and (member['last_name'].lower() in official['name'].lower()):
+                return True
+
+    return False
 
 # WEBAPP
 
@@ -83,19 +123,74 @@ if 'senate_members' not in st.session_state:
 if 'house_members' not in st.session_state:
     st.session_state['house_members'] = get_current_house_members()
 
-search_by = st.text_input("Search", placeholder="Search by name or state")
-
-st.title("Members")
+st.markdown("<h1 style='text-align: center;'>Welcome to BillBriefs!</h1>", unsafe_allow_html=True)
 st.markdown("---")
 
-st.header("Senate")
-with st.expander("State Senators", True):
-    for senator in st.session_state['senate_members']:
-        if search_members(search_by, senator):
-            render_member(senator)
+#Use USPS API to autofilll address or suggest address
+search_by = st.text_input("**Find your Elected Officials**", placeholder="Search by name, state, or address.")
 
-st.header("House")
-with st.expander("State Representatives", True):
-    for rep in st.session_state['house_members']:
-        if search_members(search_by, rep):
-            render_member(rep)
+st.markdown("<h2 style='text-align: center;'>Members</h2>", unsafe_allow_html=True)
+st.markdown("---")
+
+senators_by_state = {}
+reps_by_state = {}
+
+
+# Loop for appending senators in session_state to senators_by_state (Sorted by State)
+for senator in st.session_state['senate_members']:
+    if search_members(search_by, senator):
+        state = senator['state']
+        if state in STATE_DICT:
+            full_state_name = STATE_DICT[state]
+            if full_state_name not in senators_by_state:
+                senators_by_state[full_state_name] = []
+            senators_by_state[full_state_name].append(senator)
+
+# Loop for appending representatives in session_state to reps_by_state (Sorted by State)
+for rep in st.session_state['house_members']:
+    if search_members(search_by, rep):
+        state = rep['state']
+        if state in STATE_DICT:
+            full_state_name = STATE_DICT[state]
+            if full_state_name not in reps_by_state:
+                reps_by_state[full_state_name] = []
+            reps_by_state[full_state_name].append(rep)
+
+# Loop for rendering senators and representatives by state
+for state in sorted(set(senators_by_state.keys()) | set(reps_by_state.keys())):
+    with st.expander(f"**{state}**", True):
+        # Sets senators equal to senators_by_state for each state
+        senators = senators_by_state.get(state, [])
+        # Creates two columns for seantors and representatives
+        col1, col2 = st.columns(2)
+        # If senator 1 exists, render senator 1 in Column 1
+        with col1:
+            if len(senators) > 0:
+                render_member(senators[0])
+        # If senator 2 exists, render senator 2 in Column 2
+        with col2:
+            if len(senators) > 1:
+                render_member(senators[1])
+        # Sets representatives equal to reps_by_state for each state
+        reps = reps_by_state.get(state, [])
+        # Sorts representatives by accessnding district number
+        sorted_reps = sorted(reps, key=lambda x: (int(x['district']) if 'district' in x and x['district'].isdigit() else 0, x.get('at_large')))
+        # Loops through sorted representatives and renders them in alternating columns
+        for i, rep in enumerate(sorted_reps):
+            # Checks if district is a number
+            if 'district' in rep and rep['district'].isdigit():
+                # If district is even, render in Column 1
+                if i % 2 == 0:
+                    with col1:
+                        render_member(rep)
+                # If district is odd, render in Column 2
+                else:
+                    with col2:
+                        render_member(rep)
+            # If district is not a number, render in Column 1
+            else:
+                with col1:
+                    render_member(rep)
+                
+
+
